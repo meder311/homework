c989b7060c27e6ff251bb8d702a1395d196c4c8e
meder311 <smt031199@gmail.com>
2023-06-10 23:55:00 +0600

    First Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# """"""
+#
+# git --version
+# git config --global user.name
+# git config --global user.email
+# git init
+# git add .
+# git status
+# git commit -m "First Commit"
+# git remote add origin https://github.com/meder311/homework.git
+#
+# """
